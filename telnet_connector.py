dc8a5831be4120fbf979db13fce616749b507881
SeungSeungJanggu <victory9c@gmail.com>
2022-01-25 11:11:15 +0900

    1차 완성

--- telnet_connector.py
+++ telnet_connector.py
@@ -1,31 +1,39 @@
 import telnetlib, openpyxl
 
+user="user"
+password="1234"
 
-SERVER = [
-    ('192.168.0.2', 'user', '1234'),
-    ('192.168.0.3', 'user', '1234'),
-    ('192.168.0.2', 'user', '1234'),
-    ('192.168.0.2', 'user', '1234'),
-    ('192.168.0.2', 'user', '1234')
-]
+ip_list=[]
+wb=openpyxl.load_workbook(filename="report.xlsx")
+ws=wb.active
+ws.column_dimensions['B'].width=80
+for cell in ws['A']: #ip 열
+    ip_list.append(cell.value)
+   
+print(ip_list)
+ 
 tn=telnetlib.Telnet()
-for host, user, password in SERVER:  
+for i in range (1,len(ip_list)): 
     try:
-        tn.open(host,23,2) # 2초 안에 로그인
+        tn.open(ip_list[i],23,2)
 
-    except: #안되면 에러
+    except:
         tn.close()
         print("Connection Error")
     
     else:
-        tn = telnetlib.Telnet(host) #텔넷 로그인
+        tn = telnetlib.Telnet(ip_list[i])
         tn.read_until(b"Username: ",3)
         tn.write(user.encode('ascii') + b'\r\n')
         tn.read_until(b"Password: ",3)
         tn.write(password.encode('ascii') + b'\r\n')
-        tn.write(b' show ntp server status\n') #명령어
+        tn.write(b' show ntp server status\n')
         tn.write(b' exit\n')
-
-
-        print(tn.read_all().decode('ascii'))
-
+        command = tn.read_all().decode('ascii')
+        print(command)
+        wb=openpyxl.load_workbook(filename="report.xlsx")
+        ws=wb.active
+        ws.cell(row=2+(i-1),column=2).value = command # 2열 + ( i(1부터 시작) - 1) 이렇게 해야 2열부터 시작 
+        ws.row_dimensions[2+(i-1)].height = 140
+        #ws.cell(row=2+(i-1),column=2).alignment = align_center
+        wb.save(filename="report.xlsx")
