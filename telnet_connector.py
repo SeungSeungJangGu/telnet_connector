f4b2ff15c518c0ef2a6df96e5c6b7deca892b679
SeungSeungJanggu <victory9c@gmail.com>
2022-01-25 11:13:25 +0900

    1차 완성

--- telnet_connector.py
+++ telnet_connector.py
@@ -4,7 +4,7 @@ user="user"
 password="1234"
 
 ip_list=[]
-wb=openpyxl.load_workbook(filename="report.xlsx")
+wb=openpyxl.load_workbook(filename="report.xlsx") #엑셀에 A열 2행부터 아이피 적어둬야 ip_list에 넣을수있음
 ws=wb.active
 ws.column_dimensions['B'].width=80
 for cell in ws['A']: #ip 열
@@ -33,7 +33,7 @@ for i in range (1,len(ip_list)):
         print(command)
         wb=openpyxl.load_workbook(filename="report.xlsx")
         ws=wb.active
-        ws.cell(row=2+(i-1),column=2).value = command # 2열 + ( i(1부터 시작) - 1) 이렇게 해야 2열부터 시작 
+        ws.cell(row=2+(i-1),column=2).value = command # 2행 + ( i(1부터 시작) - 1) 이렇게 해야 2행부터 시작 
         ws.row_dimensions[2+(i-1)].height = 140
         #ws.cell(row=2+(i-1),column=2).alignment = align_center
         wb.save(filename="report.xlsx")
